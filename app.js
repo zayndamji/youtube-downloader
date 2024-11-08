590a3810e98304218736c0701b54be4bace76874
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2024-11-07 20:29:07 -0800

    css and js init

--- app.js
+++ app.js
@@ -5,4 +5,12 @@ app.get('/', (req, res) => {
   res.sendFile('index.html', { root: __dirname });
 });
 
+app.get('/styles.css', (req, res) => {
+  res.sendFile('styles.css', { root: __dirname });
+});
+
+app.get('/script.js', (req, res) => {
+  res.sendFile('script.js', { root: __dirname });
+});
+
 app.listen(9356);
