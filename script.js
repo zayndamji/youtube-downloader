bca0f70f9149b2b3908f33071740a81d356372a2
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2024-11-08 13:29:48 -0800

    adds thumbnail, title and channel

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 document.getElementById('youtube-submit').addEventListener('click', getVideoInfo);
 
 async function getVideoInfo() {
+  document.getElementById('youtube-video-title').textContent = 'Loading...';
+  document.getElementById('youtube-video-channel').textContent = '';
+  
+  document.getElementById('youtube-video-thumbnail').src = '';
+  document.getElementById('youtube-video-thumbnail').classList.remove('active');
+
   const url = document.getElementById('youtube-input').value;
   console.log(url);
 
@@ -15,4 +21,17 @@ async function getVideoInfo() {
   const json = await res.json();
 
   console.log(json);
+
+  if (json.error) {
+    document.getElementById('youtube-video-title').textContent = 'An error occured. Please try again.';
+    return;
+  }
+
+  const { formats, videoDetails } = json;
+
+  document.getElementById('youtube-video-title').textContent = videoDetails.title;
+  document.getElementById('youtube-video-channel').textContent = videoDetails.ownerChannelName;
+
+  document.getElementById('youtube-video-thumbnail').src = videoDetails.thumbnails.slice(-1)[0].url;
+  document.getElementById('youtube-video-thumbnail').classList.add('active');
 }
